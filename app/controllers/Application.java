793b066110762fe1e9711b5a22be7a75dfb9ca72
nicogiard <nicogiard@gmail.com>
2010-08-31 18:55:10 +0200

    Initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.apache.commons.lang.StringUtils;
+
+import play.mvc.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
